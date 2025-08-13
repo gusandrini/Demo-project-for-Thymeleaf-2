37121a072092e73a3044cd7ed800388920e918dc
gusandrini <gustavosandrini92@gmail.com>
2025-08-12 22:28:18 -0300

    criando CRUD

--- src/main/java/br/com/fiap/spring_mvc/service/LivroService.java
+++ src/main/java/br/com/fiap/spring_mvc/service/LivroService.java
@@ -1,7 +1,34 @@
 package br.com.fiap.spring_mvc.service;
 
+import br.com.fiap.spring_mvc.entity.Livro;
+import br.com.fiap.spring_mvc.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LivroService {
+
+    private final LivroRepository livroRepository;
+
+    @Autowired
+    public LivroService(LivroRepository livroRepository){
+
+    }
+    //CRUD
+    public Livro createLivro(Livro livro){
+        return livroRepository.save(livro);
+    }
+
+    public Livro readLivro(longId){
+        return livroRepository.findById(id).orElse(null);
+    }
+
+    public Livro updateLivro(Livro livro){
+        return livroRepository.save(livro);
+    }
+
+    public void deleteLivro(Long id){
+        livroRepository.deleteById(id);
+    }
+
 }
